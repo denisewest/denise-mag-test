ba8cb5615e46c6ebece1bef5ba6f4f8f38001427
Denise Vestman <dv222dh@student.lnu.se>
2022-04-24 19:57:41 +0200

    Add enums to validate user and product type

--- refactor/price-calculator.js
+++ refactor/price-calculator.js
@@ -1,21 +1,46 @@
-// userType, 0 = normal, 1 = company
-// productType, 0 = new product, 1 = old product
-// price, the price of the product
+// Enums for user types
+const UserTypes = Object.freeze({
+  Normal: 0,
+  Company: 1,
+});
+
+// Enums for product types
+const ProductTypes = Object.freeze({
+  New: 0,
+  Old: 1,
+});
+
+/**
+ * Calculates the current price of a product for a type of user.
+ *
+ * @param {number} userType - the type of a user
+ * @param {number} productType - the type of a product
+ * @param {number} price - the price of a productType
+ * @param {Date Object} publishedDate - the publishing date of a product
+ * @returns {number} the current price of a product
+ */
 const calculatePrice = (userType, productType, price, publishedDate) => {
-  // if (userType)
   try {
+    if (!Object.values(UserTypes).includes(userType)) {
+      throw new Error('Invalid user type.');
+    }
+
+    if (!Object.values(ProductTypes).includes(productType)) {
+      throw new Error('Invalid product type.');
+    }
+
     const today = new Date().toDateString();
     let rebateAmount = 0;
     let productTypePrice = 35; // Old product price
 
-    if (productType === 0) { // New product
+    if (productType === ProductTypes.New) {
       if (publishedDate.toDateString() === today) { // Published today
         rebateAmount = 10; // Add 10 rebate amount
       }
       productTypePrice -= 10; // Subtract 10 from old product price
     }
 
-    if (userType === 1) { // Company user
+    if (userType === UserTypes.Company) {
       rebateAmount += 5; // Add 5 company rebate amount
     }
 
