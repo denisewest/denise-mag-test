736a349a42800c026604a4e33091d61bc9584409
Denise Vestman <dv222dh@student.lnu.se>
2022-04-26 11:55:37 +0200

    Add form to add new product

---
+++ src/components/Form/Form.tsx
@@ -0,0 +1,50 @@
+import { FormEvent, useState } from 'react'
+import './Form.css'
+import Product from '../../models/product'
+
+interface FormProps {
+  onAddProduct: (product: Product) => void
+}
+
+function Form(props: FormProps) {
+  const [productName, setProductName] = useState('')
+  const [productType, setProductType] = useState(0)
+  const [productPrice, setProductPrice] = useState(0)
+  const [publishedDate, setPublishedDate] = useState('')
+  const { onAddProduct } = props
+
+  const submitValues = (event: FormEvent) => {
+    event.preventDefault()
+
+   const product: Product = {
+    name: productName,
+    type: productType,
+    price: productPrice,
+    publishedDate: new Date(publishedDate)
+   }
+
+   onAddProduct(product)
+  }
+
+  return (
+    <div id='form'>
+      <h6>Add product</h6>
+      <form method='post' autoComplete='off'>
+        <label htmlFor='name'>Product name</label>
+        <input type='text' id='name' required onChange={e => setProductName(e.target.value)} /><br/>
+        <label htmlFor='type'>Product type</label>
+        <select onChange={e => setProductType(Number(e.target.value))}>
+          <option value='0' className='type'>New</option>
+          <option value='1' className='type'>Old</option>
+        </select><br/>
+        <label htmlFor='price'>Price</label>
+        <input type='number' id='price' min='0' required onChange={e => setProductPrice(Number(e.target.value))}/><br/>
+        <label htmlFor='date'>Published date</label>
+        <input type='date' id='date' required onChange={e => setPublishedDate(e.target.value)}/><br/>
+        <input onClick={submitValues} type='submit' className='button' value='Add' />
+      </form>
+    </div>
+  )
+}
+
+export default Form
--- src/components/Products/Products.tsx
+++ src/components/Products/Products.tsx
@@ -1,29 +1,26 @@
 import { useState } from 'react'
+import './Products.css'
 import getProducts from '../../utils/get-products'
+import Product from '../../models/product'
 import ProductItem from '../ProductItem/ProductItem'
+import Form from '../Form/Form'
 import UserRadio from '../UserRadio/UserRadio'
 import User from '../../models/user'
-import './Products.css'
 
 function Products() {
   const [userData, setUserData] = useState<User>(User.Normal)
   const [totalPrice, setTotalPrice] = useState<Map<string, number>>(new Map<string, number>())
-
-  const products = getProducts()
+  const [products, setProducts] = useState<Product[]>(getProducts())
 
   const updateTotalPrice = (key: string, value: number) => {
     const changedTotal = totalPrice.set(key, value)
     setTotalPrice(new Map(changedTotal))
   }
 
-  console.log('totalPrice ', totalPrice)
-
   const calculateTotalPrice = (): number => {
-    console.log(totalPrice.values())
     const total = Array.from(totalPrice.values()).reduce(
       (previousValue: number, currentValue: number) => previousValue + currentValue, 0
       )
-      console.log(total)
       return total
   }
 
@@ -46,9 +43,14 @@ function Products() {
             )
           })}
         <h5>Your total {calculateTotalPrice()} SEK</h5>
+        <Form onAddProduct={(product) => {
+          const currentProducts = Array.from(products)
+          currentProducts.push(product)
+          setProducts(currentProducts)
+        }} />
       </div>
     </div>
   )
 }
 
-export default Products;
+export default Products
